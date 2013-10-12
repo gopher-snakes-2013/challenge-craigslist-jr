95d11def810423efde87b0c551896d3db50a52ab
Apprentice <apprentice@dbc09.local>
2013-10-12 14:35:54 -0700

    created tests for posting sales and viewing listings * two links take guests to different parts of the site * new listings page and view listings page

--- hedgeslist_app.rb
+++ hedgeslist_app.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/post-a-sale' do
+  erb :post_a_sale
+end
+
+get '/view-all-listings' do
+  erb :all_listings
+end
--- spec/feature_tests/guest_homepage_spec.rb
+++ spec/feature_tests/guest_homepage_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
-feature "guest visits the homepace" do
+feature "guest visits the homepage" do
   scenario "they see HegesList" do
     visit "/"
     expect(page).to have_content "HedgesList"
   end
+  
   scenario "they see some content" do
     visit "/"
     expect(page).to have_content "A place where you can sell all the things!"
   end
+
+  scenario "visit new page from home page to create post" do
+      visit "/"
+      click_on "post something"
+      expect(page).to have_content "Post all the things!"
+  end
+
+  scenario "visit all sales page from home page" do
+    visit "/"
+    click_on "browse our listings"
+    expect(page).to have_content "Listings of all the things!"
+  end
+
 
 end
