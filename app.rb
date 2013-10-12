554c3fa9dd5b0e8675a3d89fd482ec8b2e169456
Robert Taraya <robert.taraya@gmail.com>
2013-10-12 15:28:28 -0700

    create views for item

--- app.rb
+++ app.rb
@@ -6,5 +6,21 @@ class Item < ActiveRecord::Base
 end
 
 get '/' do
+  @items=Item.all
   erb :index
+end
+
+get '/item/new' do
+  erb :item_new
+end
+
+get '/item/:id' do
+  @item = Item.find(params[:id])
+  erb :item_view
+end
+
+post '/item/new' do
+  item = Item.create(title: params[:title], price: params[:price], description: params[:desc])
+
+  redirect "/item/#{item.id}"
 end
