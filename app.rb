2285c8c2a78df616c802efc49fd32c434659b5eb
Robert Taraya <robert.taraya@gmail.com>
2013-10-12 14:13:27 -0700

    add item model to app.rb

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/activerecord'
 set :database, ENV['DATABASE_URL'] || "sqlite3:///db/craigslist_jr.db"
 
+class Item < ActiveRecord::Base
+end
 
 get '/' do
   erb :index
