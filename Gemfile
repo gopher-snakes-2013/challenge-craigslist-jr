2eaea32fe501c0884f2856d43eed88c53fd66be8
Annie Sing <anniesing1014@gmail.com>
2013-10-12 15:51:53 -0700

    not working database, switch computers

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'sinatra'
+gem 'sinatra-activerecord'
+gem 'shotgun'
+gem 'pg'
+
+
+group :development do
+  gem 'rspec'
+  # LOOK AT ME WHEN DEPLOYING
+  gem "sqlite3"
+
+end
---
+++ specs/craiglist_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'spec_helper'
+
+# describe Item do
+#   before(:each) do
+#     @item = Item.new
+#   end
+
+#   it "should have a title" do
+#     expect(@item.title).to eq("bike")
+#   end
+
+#   it "should have a description" do
+#     expect(@itme.description).to eq("dat purple bike doe")
+#   end
+
+end
