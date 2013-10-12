61a201113a0c504929a8d5d570d3958477f3ff47
Jeff Edwards <js_edwards@yahoo.com>
2013-10-12 08:39:23 -0700

    update post test to include description and price

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe Post do 
-  let(:post) { Post.create(title: "Example Bike", description: "This example bike is in good shape.", price: "$30")}
+  let(:post) { Post.create(title: "Example Bike", 
+                          description: "This example bike is in good shape.",
+                          price: "$30")}
   it "has to have a title, description and price" do
     expect(post.title).to eql "Example Bike"
+    expect(post.description).to eql "This example bike is in good shape."
+    expect(post.price).to eql "$30"
   end
 end
 
