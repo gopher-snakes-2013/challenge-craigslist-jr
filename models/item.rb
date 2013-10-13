98dcbebd0294cde35f711f1a60401eefec9e73a9
Taylor Hudson <taylor.a.hudson@gmail.com>
2013-10-13 16:11:27 -0700

    fixed missed change to item_key

--- models/item.rb
+++ models/item.rb
@@ -1,4 +1,4 @@
 class Item < ActiveRecord::Base
-  validates :title, :description, :price, :secure_key, :presence => true
+  validates :title, :description, :price, :item_key, :presence => true
   validates :item_key, :uniqueness => true
 end
