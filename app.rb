064185b1c15d65e525d8710243c56d3ad1a924a8
Robert Taraya <robert.taraya@gmail.com>
2013-10-13 16:37:19 -0700

    finish project...final commits

--- app.rb
+++ app.rb
@@ -5,9 +5,12 @@ set :database, ENV['DATABASE_URL'] || "sqlite3:///db/craigslist_jr.db"
 enable :sessions
 
 class Item < ActiveRecord::Base
+
   belongs_to :user
 end
+
 class User < ActiveRecord::Base
+
   has_many :items
 end
 
@@ -17,17 +20,40 @@ get '/' do
 end
 
 get '/login' do
+
   erb :login
 end
 
 post '/login' do
-  user = User.create(username: params[:username], password: params[:password])
-  session[:user_id] = user.id if user
+  user = User.find_by(username: params[:username])
+  if user && user.password == params[:password]
+    session[:user_id] = user.id
+    session[:username] = user.username
+    redirect "/user/#{user.id}"
+  else
+    erb :login
+  end
+end
 
+post '/signup' do
+  User.create(username: params[:username], password: params[:password])
+  session[:user_id] = user.id
+  redirect "/user/#{user.id}"
+end
+
+get '/logout' do
+  session.clear
   redirect '/'
 end
 
+get '/user/:user_id' do
+  @user = User.find_by(id: params[:user_id])
+
+  erb :user_view
+end
+
 get '/item/new' do
+
   erb :item_new
 end
 
@@ -38,8 +64,9 @@ end
 
 post '/item/new' do
   item_url = create_secure_url
-  item = Item.create(title: params[:title], price: params[:price], description: params[:desc], url: item_url)
-
+  item = Item.new(title: params[:title], price: params[:price], description: params[:desc], url: item_url)
+  item.user_id = session[:user_id] if session[:user_id]
+  item.save
   redirect "/item/#{item.url}"
 end
 
@@ -58,6 +85,12 @@ post '/item/update/:item_url' do
   redirect "/item/#{@item.url}"
 end
 
+get '/item/delete/:item_url' do
+  item = find_item_from(params[:item_url])
+  item.destroy
+  redirect "/user/#{session[:user_id]}"
+end
+
 helpers do
   def create_secure_url
     require 'securerandom'
@@ -65,6 +98,6 @@ helpers do
   end
 
   def find_item_from(secure_url)
-    Item.where("url = ?", secure_url).first
+    Item.find_by(url: secure_url)
   end
 end
