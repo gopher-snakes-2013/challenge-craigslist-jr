254333499303ccf26db4431f143f54adf6b8a532
Robert Taraya <robert.taraya@gmail.com>
2013-10-12 17:30:08 -0700

    add secure url, allow edit/update item

--- app.rb
+++ app.rb
@@ -14,13 +14,40 @@ get '/item/new' do
   erb :item_new
 end
 
-get '/item/:id' do
-  @item = Item.find(params[:id])
+get '/item/:item_url' do
+  @item = find_item_from(params[:item_url])
   erb :item_view
 end
 
 post '/item/new' do
-  item = Item.create(title: params[:title], price: params[:price], description: params[:desc])
+  item_url = create_secure_url
+  item = Item.create(title: params[:title], price: params[:price], description: params[:desc], url: item_url)
 
-  redirect "/item/#{item.id}"
+  redirect "/item/#{item.url}"
+end
+
+get '/item/edit/:item_url' do
+  @item = find_item_from(params[:item_url])
+  erb :item_edit
+end
+
+post '/item/update/:item_url' do
+  @item = find_item_from(params[:item_url])
+  @item.title = params[:title]
+  @item.price = params[:price]
+  @item.description = params[:desc]
+  @item.save
+
+  redirect "/item/#{@item.url}"
+end
+
+helpers do
+  def create_secure_url
+    require 'securerandom'
+    SecureRandom.hex
+  end
+
+  def find_item_from(secure_url)
+    Item.where("url = ?", secure_url).first
+  end
 end
