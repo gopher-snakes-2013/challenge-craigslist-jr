2c33a39576a5bd06db1616cabb2c8164979a8768
Taylor Hudson <taylor.a.hudson@gmail.com>
2013-10-13 15:51:12 -0700

    added app.rb with post route

--- app.rb
+++ app.rb
@@ -8,3 +8,8 @@ get '/' do
   @items = Item.all
   erb :index
 end
+
+post '/new_item' do
+  Item.create(title: params[:new_item_title], description: params[:new_item_desc], price: params[:new_item_price])
+  redirect to '/'
+end
