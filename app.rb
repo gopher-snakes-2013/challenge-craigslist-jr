9072509c5f56f83b7802e578d6e28b7260deeb35
carter <carter.sowers@gmail.com>
2013-10-13 17:41:20 -0700

    basic working app for creating/viewing posts

--- app.rb
+++ app.rb
@@ -1,15 +1,44 @@
-$LOAD_PATH.unshift(File.expand_path('.'))
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/flash'
 require 'shotgun'
-require 'models/item'
-#require 'models/user'
+require './models/item'
+require './models/user'
 
 begin
-require 'dotenv'
-Dotenv.load
-set :database, "sqlite3:///craigs.db"
-#ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  require 'dotenv'
+  Dotenv.load
 rescue LoadError
 end
+
+#set :database, ENV['DATABASE_URL']
+set :database, "sqlite3:///craigs.db"
+enable :sessions
+
+get '/' do
+  @items = Item.all
+  erb :index
+end
+
+post '/' do
+  secret_url = SecureRandom.urlsafe_base64
+  new_item = Item.new(title: params[:title], price: params[:price], description: params[:description], edit_url: secret_url)
+  if new_item.save
+    redirect '/'
+  else
+    flash[:error] = "Please fill in all the fields!"
+    redirect '/'
+  end
+end
+
+get '/posts/:id' do
+  @item = Item.find_by(id: params[:id])
+  erb :post
+end
+
+get '/posts/:id/:edit_url' do
+  @item = Item.find_by(id: params[:id])
+  erb :edit_post
+end
+
 
