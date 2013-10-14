df2d23f29ed456ae87e9cfeb05c3458a5332abc5
Apprentice <apprentice@dbc31.local>
2013-10-13 17:52:14 -0700

    WIP - Test guest can fill out required fields

--- spec/features/guest_may_list_items_for_sale_spec.rb
+++ spec/features/guest_may_list_items_for_sale_spec.rb
@@ -2,5 +2,16 @@ require 'spec_helper'
 
 feature "Guest may list items for sale" do
   scenario "Guest fills out required fields"
+    visit '/'
+    click_on "Get Rid of Your Shit!"
+
+    fill_in "title", with: "Giant Stone Monkey Head"
+    fill_in "desc", with: "This is the second biggest monkey head I've ever sold!"
+    fill_in "price", with: "$450"
+
+    click_on "I Don't Want This Anymore!"
+
+    expect(page).to have_content("")
+
   scenario "Guest submits information to database"
 end
